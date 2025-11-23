8d2ed0ef297c1d8abc4810019ba0e7dfdfaae8a8
AllowOwl <2016scoyvoker@mail.ru>
2025-11-23 23:55:25 +0300

    Updated forms.py

--- pastebin/accounts/forms.py
+++ pastebin/accounts/forms.py
@@ -3,11 +3,36 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите ваш email'
+        })
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите имя пользователя'
+        })
+    )
+    bio = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Расскажите о себе...',
+            'rows': 3
+        })
+    )
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password1', 'password2', 'bio')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Введите пароль'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Подтвердите пароль'})
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -15,4 +40,23 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-    
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'bio', 'role')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'role': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Ограничиваем выбор ролей для не-админов
+        if not self.instance.is_admin():
+            self.fields['role'].choices = [
+                (CustomUser.Role.USER, 'Пользователь'),
+                (CustomUser.Role.MODDERATOR, 'Модератор'),
+            ]
